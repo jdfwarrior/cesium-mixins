bed01f684a205648460c936b0d0ea631593c9b7a
David Ferguson <david@ferg.co>
2024-07-19 23:09:01 -0500

    allow toggling tooltips on/off

--- src/tooltips.ts
+++ src/tooltips.ts
@@ -2,41 +2,76 @@ import { Viewer, ScreenSpaceEventHandler, Entity, Cartesian2 } from "cesium";
 import { Helper } from "./lib/helper";
 import { Handler } from "./lib/handler";
 
-export default function (viewer: Viewer) {
-  const ele = viewer.canvas;
-  const handler = new Handler(ele);
+declare module "cesium" {
+  interface Viewer {
+    tooltips: {
+      enable: () => void
+      disable: () => void
+      enabled: boolean
+    }
+  }
+}
 
-  // create a new tooltip, styled so that it is initially hidden and is slightly smaller than the others
-  const helper = new Helper(viewer, {
-    styles: { display: "none", fontSize: "12px", lineHeight: "12px" },
-  });
-  helper.show();
+class Tooltips {
+  ele: HTMLCanvasElement
+  handler: Handler
+  helper: Helper
+  enabled: boolean
+
+  constructor(public viewer: Viewer) {
+    this.ele = viewer.canvas;
+    this.handler = new Handler(this.ele);
+    this.enabled = false
+
+    const options = {
+      styles: { display: "none", fontSize: "12px", lineHeight: "12px" },
+    }
+
+    // create a new tooltip, styled so that it is initially hidden and is slightly smaller than the others
+    this.helper = new Helper(this.viewer, options);
+  }
+
+  /**
+   * Enable listening for mouse events and showing the tooltips
+   */
+  enable() {
+    this.handler.on("mouse_move", (event: ScreenSpaceEventHandler.MotionEvent) => this.onMove(event));
+    this.enabled = true
+  }
+
+  /**
+   * Disable listening for mouse events and showing the tooltips
+   */
+  disable() {
+    this.handler.off('mouse_move')
+    this.enabled = false
+  }
 
   /**
    * Shows the tooltip at the cursor location (plus an offset) for the provided entity
    * @param entity hovered entity
    * @param at cartesian position of the cursor
    */
-  function show(entity: Entity, at: Cartesian2) {
+  show(entity: Entity, at: Cartesian2) {
     const { name } = entity;
     const { x, y } = at;
 
     if (!name) {
-      hide();
+      this.hide();
     } else {
-      helper.show();
+      this.helper.show();
       const left = x + 20 + "px";
       const top = y - 25 + "px";
-      helper.update(name);
-      helper.setStyles({ display: "block", top, left });
+      this.helper.update(name);
+      this.helper.setStyles({ display: "block", top, left });
     }
   }
 
   /**
    * Hides the tooltip when no entity is hovered or the entity has no name
    */
-  function hide() {
-    if (helper.isVisible()) helper.hide();
+  hide() {
+    if (this.helper.isVisible()) this.helper.hide();
   }
 
   /**
@@ -45,25 +80,38 @@ export default function (viewer: Viewer) {
    * if not, hide the tooltip
    * @param event mouse move event
    */
-  function onMove(event: ScreenSpaceEventHandler.MotionEvent) {
+  onMove(event: ScreenSpaceEventHandler.MotionEvent) {
     const position = event.endPosition;
-    const hovering = viewer.scene.drillPick(position);
+    const hovering = this.viewer.scene.drillPick(position);
     const [primitive] = hovering;
 
     if (primitive) {
       const entity = primitive.id;
-      show(entity, position);
+      this.show(entity, position);
     } else {
-      hide();
+      this.hide();
     }
   }
 
-  /**
-   * initialize listener
-   */
-  function init() {
-    handler.on("mouse_move", onMove);
-  }
+}
 
-  init();
+interface TooltipOptions {
+  enabled: boolean
+}
+
+export default function (viewer: Viewer, options: TooltipOptions = { enabled: false }) {
+  const tooltips = new Tooltips(viewer)
+
+  if (options.enabled) tooltips.enable()
+
+  Object.defineProperties(Viewer.prototype, {
+    tooltips: {
+      value: {
+        enable: () => tooltips.enable(),
+        disable: () => tooltips.disable(),
+        get enabled() { return tooltips.enabled }
+      },
+      writable: true
+    }
+  })
 }
